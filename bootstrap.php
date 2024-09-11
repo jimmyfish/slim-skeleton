8d0e1b548cb6c2750c2070f42d327000dbacde45
Jimmy Fish <killcoder212@gmail.com>
2024-09-12 03:12:36 +0700

    move single responsibility bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,12 @@
 <?php
 
-use Doctrine\DBAL\DriverManager;
-use Doctrine\ORM\EntityManager;
+use UMA\DIC\Container;
 use Doctrine\ORM\ORMSetup;
+use Slim\Factory\AppFactory;
+use Doctrine\ORM\EntityManager;
+use Doctrine\DBAL\DriverManager;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
-use UMA\DIC\Container;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -33,4 +34,24 @@ $container->set(EntityManager::class, static function (Container $c): EntityMana
     return new EntityManager($connection, $config);
 });
 
-return $container;
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+// for dependency injection
+// $app = \DI\Bridge\Slim\Bridge::create($container);
+
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+require __DIR__ . '/routes/api.php';
+
+return $app;
